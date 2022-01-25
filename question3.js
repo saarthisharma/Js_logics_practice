b3b89c4077bc24b3aa2af635d32825a8528e81d6
saarthisharma <saarthi.sharma@dianapps.com>
2022-01-25 18:45:47 +0530

    new code added

--- question3.js
+++ question3.js
@@ -10,3 +10,15 @@ for(i=0;i<array.length;i++)
    }
 }
 console.log(object)
+
+console.log(object)
+let key = Object.keys(object)
+console.log(key)
+let values = Object.values(object)
+console.log(values)
+// (...) is spread operator
+//Passing elements of the array as arguments to the Math Object
+let max = Math.max(...values)
+console.log("max value is" ,max)
+let min = Math.min(...values)
+console.log("min value is",min)
